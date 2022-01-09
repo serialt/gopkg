78f48fe30ef46b70ade5511dc466e70962ab3703
serialt <serialt@qq.com>
2022-01-09 22:21:24 +0800

    feat: add env

--- env.go
+++ env.go
@@ -108,3 +108,20 @@ func EnvGetFloat64D(envName string, defaultValue float64) float64 {
 func EnvGetBool(envName string) bool {
 	return strings.EqualFold(os.Getenv(envName), "true")
 }
+
+// Environ like os.Environ, but will returns key-value map[string]string data.
+func Environ() map[string]string {
+	envList := os.Environ()
+	envMap := make(map[string]string, len(envList))
+
+	for _, str := range envList {
+		nodes := strings.SplitN(str, "=", 2)
+
+		if len(nodes) < 2 {
+			envMap[nodes[0]] = ""
+		} else {
+			envMap[nodes[0]] = nodes[1]
+		}
+	}
+	return envMap
+}
--- env_test.go
+++ env_test.go
@@ -1,8 +1,11 @@
 package gopkg
 
 import (
+	"fmt"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEnvCommon_Get(t *testing.T) {
@@ -87,4 +90,35 @@ func TestEnvCommon_GetBool(t *testing.T) {
 	t.Log("HELLO =", EnvGetBool("HELLO"))
 	_ = os.Setenv("HELLO", "WORLD")
 	t.Log("HELLO =", EnvGetBool("HELLO"))
+}
+
+const (
+	TestEnvName         = "TEST_GOUTIL_ENV"
+	TestNoEnvName       = "TEST_GOUTIL_NO_ENV"
+	TestEnvValue        = "1"
+	DefaultTestEnvValue = "1"
+)
+
+func TestGetenv(t *testing.T) {
+	testutil.MockEnvValues(map[string]string{
+		TestEnvName: TestEnvValue,
+	}, func() {
+		envValue := Getenv(TestEnvName)
+		assert.Equal(t, TestEnvValue, envValue, "env value not equals")
+		envValue = Getenv(TestNoEnvName, DefaultTestEnvValue)
+		assert.Equal(t, DefaultTestEnvValue, envValue, "env value not default")
+	})
+}
+
+func TestEnviron(t *testing.T) {
+	testutil.MockOsEnv(map[string]string{
+		TestEnvName: TestEnvValue,
+	}, func() {
+		envValue := Getenv("not_exist")
+		assert.Equal(t, "", envValue)
+
+		fmt.Println("os.Environ:", os.Environ())
+		fmt.Println("new Environ:", Environ())
+		assert.Contains(t, Environ(), TestEnvName)
+	})
 }
